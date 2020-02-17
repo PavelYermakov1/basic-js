bb8338489b1de048d1b5299e1d2b7bae1fcb70a9
Pavel Yermakov <pavelak137@mail.ru>
2020-02-17 20:40:10 +0300

    feat: complited hanoi tower and what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (Object.keys(date).length > 0) {
+    throw new Error();
+  }
+
+  if(date.getMonth() >= 2 && date.getMonth() < 5) {
+    return 'spring';
+  } else if(date.getMonth() >= 5 && date.getMonth() < 8) {
+    return 'summer';
+  } else if(date.getMonth() >= 8 && date.getMonth() < 11) {
+    return 'autumn';
+  } else {
+    return 'winter';
+  }
 };
