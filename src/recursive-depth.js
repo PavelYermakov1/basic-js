77614acf65791bd7202879fad5e11ff34bc7a4e9
Pavel Yermakov <pavelak137@mail.ru>
2020-02-18 21:39:14 +0300

    feat: complited recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    calculateDepth(arr) {
+        if (!Array.isArray(arr)) {
+            return 0;
+        } else {
+            let count = arr.reduce((acc,cur) => {
+                return acc = Math.max(this.calculateDepth(cur), acc)
+              }, 0);
+            return count + 1;
+        }
+    }   
 };
