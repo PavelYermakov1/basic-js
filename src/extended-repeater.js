24b0aef99b855bbdf10218b9c401cf97801b0d25
Pavel Yermakov <pavelak137@mail.ru>
2020-02-18 23:35:48 +0300

    feat: complited extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.addition !== undefined ? options.addition : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  let middleResult = '';
+  let finalResult = '';
+  let i = 1;
+  let j = 1;
+  while(i < additionRepeatTimes) {
+    middleResult = `${middleResult}${addition}${additionSeparator}`
+    i++;
+  }
+  middleResult += addition;
+  while(j < repeatTimes) {
+    finalResult = `${finalResult}${str}${middleResult}${separator}`
+    j++;
+  }
+  return finalResult += `${str}${middleResult}`
 };
   
